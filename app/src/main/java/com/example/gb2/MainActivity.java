9aa922d30954d9fdac28b503f297b979bd78db95
vakurov <temavakurov1@icloud.com>
2022-04-19 10:00:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gb2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.gb2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        
+    }
+
+
+
+
+}
